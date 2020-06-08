88159ee8e0f2d9f8e008497dac5a9dd406f71ed3
chingachcook <batirbaevchingiz@gmail.com>
2020-06-08 17:45:58 +0600

    feature-7 fixes bugs

--- src/main/java/com/example/ecommerce/repository/PizzaProductRepository.java
+++ src/main/java/com/example/ecommerce/repository/PizzaProductRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PizzaProductRepository extends JpaRepository<PizzaProduct, Integer> {
     List<PizzaProduct> findAllByPrice(double price, Pageable pageable);
+
+    Optional<PizzaProduct> findById(long id);
 }
